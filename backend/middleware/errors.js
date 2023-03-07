0d3d90db238168b21d14b3d74ff5831851916215
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2023-03-07 10:48:35 +0530

    update error.js file

--- backend/middleware/errors.js
+++ backend/middleware/errors.js
@@ -2,10 +2,25 @@ const ErrorHandler = require('../utils/errorHandler');
 
 module.exports=(err, req, res, next)=>{
     err.statusCode = err.statusCode || 500;
-    err.message = err.message ||'Internal server Error';
+    
+    if (process.env.NODE_ENV === 'DEVELOPMENT'){
+        res.status(err.statusCode).json({
+            success:false,
+            error: err,
+            errMessage:err.message,
+            stack: err.stack
+        })
+    }
 
-    res.status(err.statusCode).json({
-        success:false,
-        error:err
-    })
+    if(process.env.NODE_ENV === 'PRODUCTION'){
+        let error = {...err}
+
+        error.message = err.message
+
+        res.status(error.statusCode).json({
+            success:false,
+            message:error.message || 'Internal Server error'
+        })
+    }
+   
 }

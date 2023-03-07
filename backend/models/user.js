0cdebaaaab491eefc7770ac9c74ac6d5057e3685
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2023-03-08 01:26:43 +0530

    encrypting user password

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -40,5 +41,14 @@ const userSchema = new mongoose.Schema({
     resetPasswordToken: String,
     resetPasswordExpire: Date
 
+})
+
+// Encrypting Password before saving user
+userSchema.pre( 'save', async function (next) {
+    if (!this.isModified('password')){
+        next()
+    }
+
+    this.password = await bcrypt.hash(this.password, 10)
 })
 module.exports =mongoose.model('User', userSchema)

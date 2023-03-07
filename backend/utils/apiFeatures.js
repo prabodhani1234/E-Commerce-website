a71984593542ac6de345ea6a420233e383a048f7
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2023-03-07 21:34:57 +0530

    add search item field

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -18,6 +18,18 @@ class APIFeatures {
         this.query = this.query.find({ ...keyword});
         return this;
     }
+    filter(){
+
+        const queryCopy = { ...this.queryStr};
+
+
+        //remove fields from th query
+        const removeFields = ['keyword', 'limit', 'page']
+        removeFields.forEach(el => delete queryCopy[el]);
+
+        this.query = this.query.find(queryCopy);
+        return this;
+    }
     
 }
 

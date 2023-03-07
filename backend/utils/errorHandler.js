bc5c2aa4895485d59d8c8c03e3cdcc9456dd557b
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2023-03-07 09:44:12 +0530

    create error handler

---
+++ backend/utils/errorHandler.js
@@ -0,0 +1,11 @@
+//class of error handler
+class ErrorHandler extends Error{
+    constructor(message, statusCode){
+        super(message);
+        this.statusCode =statusCode
+
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+module.exports = ErrorHandler;

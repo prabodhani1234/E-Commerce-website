3bf547b36a4cec043119a1b5dafbb91eddfdc820
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2023-03-08 12:18:39 +0530

    generate JOSN web token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -19,8 +19,10 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
         }
     })
 
+    const token = user.getJwtToken();
+
     res.status(201).json({
         success: true,
-        user
+        token
     })
 })

a33eb3f1be0ec24d26c622907fb7179969f3dc6a
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2023-03-08 20:22:58 +0530

    create api for protect routes from unauthorized users

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router();
 
-const{ getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
+const{ 
+    getProducts, 
+    newProduct, 
+    getSingleProduct, 
+    updateProduct, 
+    deleteProduct 
 
-router.route('/products').get(getProducts);
+} = require('../controllers/productController')
+
+const { isAuthenticatedUser } = require('../middleware/auth');
+
+router.route('/products').get(isAuthenticatedUser, getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
 router.route('/admin/product/new').post(newProduct);

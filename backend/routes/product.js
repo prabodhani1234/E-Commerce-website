b182a1f3cd950996d78a0ab3fbc89d6868af6ce5
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2023-03-08 21:31:10 +0530

    authorized user roles and permission

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -10,14 +10,14 @@ const{
 
 } = require('../controllers/productController')
 
-const { isAuthenticatedUser } = require('../middleware/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
-router.route('/products').get(isAuthenticatedUser, getProducts);
+router.route('/products').get(isAuthenticatedUser, authorizeRoles('admin'), getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/admin/product/new').post(newProduct);
+router.route('/admin/product/new').post(isAuthenticatedUser, newProduct);
 router.route('/admin/product/:id')
-                    .put(updateProduct)
-                    .delete(deleteProduct);
+                    .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
+                    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
 module.exports = router;

66763db2b755cdefc02632ce500df419f7f4620c
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2023-03-10 19:05:05 +0530

    add code for handling mongoose duplicate key errors

--- backend/middleware/errors.js
+++ backend/middleware/errors.js
@@ -29,7 +29,11 @@ module.exports=(err, req, res, next)=>{
             error = new ErrorHandler(message, 400)
         }
        
-
+        //Handling Mongoose duplicate key errors
+        if(err.code === 11000){
+            const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+            error = new ErrorHandler(message, 400)
+        }
 
 
         res.status(error.statusCode).json({
